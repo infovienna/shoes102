1ee690b029ee0432bef995431eb45a4224e76c3d
infovienna <118638425+infovienna@users.noreply.github.com>
2022-11-20 08:29:18 -0800

    Add files via upload

---
+++ ibaidu1.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?5c1e5643cef5a393ac761ffac5f9da25";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
